fb9b9c926c1c562d36be5d638dceec1c2505aa23
BikashShah-404 <shahbikash4659@gmail.com>
2026-01-25 15:28:12 +0545

    implement type

--- app/main.ts
+++ app/main.ts
@@ -5,20 +5,27 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+const builtIn: Set<String> = new Set(["echo", "exit", "type"]);
+
 function givePrompt() {
-  rl.question("$ ", (command) => {
-    if (command === "exit") {
+  rl.question("$ ", (command: string) => {
+    // split(/s+/) - splits the string into parts wherever there are one or more whitespace characters.
+    const commandParts = command.trim().split(/\s+/);
+    if (commandParts[0] === "exit") {
+      const code = commandParts[1] ? parseInt(commandParts[1], 10) : 0;
       rl.close();
-      process.exit(0);
-    } else if (command.startsWith("echo ")) {
-      console.log(
-        command
-          .split(" ")
-          .filter((eachCommand, index) => index !== 0)
-          .join(" "),
-      );
+      process.exit(code);
+    } else if (commandParts[0] === "echo") {
+      console.log(commandParts.slice(1).join(" "));
+    } else if (commandParts[0] === "type") {
+      const isBuiltIn = commandParts[1] && builtIn.has(commandParts[1]);
+      if (isBuiltIn) {
+        console.log(`${commandParts[1]} is a shell builtin`);
+      } else {
+        console.log(`${commandParts[1]}: not found`);
+      }
     } else {
-      console.log(`${command}: command not found`);
+      console.log(`${commandParts[0]}: command not found`);
     }
     givePrompt();
   });
