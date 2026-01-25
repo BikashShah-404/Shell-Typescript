9eb8beb0c9b04577d2c82085423b7f013762d057
BikashShah-404 <shahbikash4659@gmail.com>
2026-01-25 14:57:54 +0545

    implement echo

--- app/main.ts
+++ app/main.ts
@@ -9,10 +9,18 @@ function givePrompt() {
   rl.question("$ ", (command) => {
     if (command === "exit") {
       rl.close();
+      process.exit(0);
+    } else if (command.startsWith("echo ")) {
+      console.log(
+        command
+          .split(" ")
+          .filter((eachCommand, index) => index !== 0)
+          .join(" "),
+      );
     } else {
       console.log(`${command}: command not found`);
-      givePrompt();
     }
+    givePrompt();
   });
 }
 givePrompt();
