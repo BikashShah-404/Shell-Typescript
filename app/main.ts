5ad60f30f17f70ca93b2e790f52b4d7e52661f2b
BikashShah-404 <shahbikash4659@gmail.com>
2026-01-28 11:25:55 +0545

    run a program

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import fs from "fs";
 import { createInterface } from "readline";
+import { execFile } from "child_process";
 
 const rl = createInterface({
   input: process.stdin,
@@ -10,33 +11,51 @@ const rl = createInterface({
 const builtIn: Set<String> = new Set(["echo", "exit", "type"]);
 const paths: String[] | undefined = process.env.PATH?.split(path.delimiter);
 
-const checkExecFileExistAndPerm = (filename: string): void => {
-  // some will return true as soon as it finds the file with the exec permission and doesn't check other paths , well that's how the path search for executable commands works.
-  const isThereAnyExecFile = paths?.some((eachPath) => {
-    // path.join() basically concats the parts of path as per the operating sysytem
-    const pathForFile = path.join(eachPath.toString(), filename);
-
-    // fs.existsSync() will synchronously check if the file exists.
-    const doesFileForCommandExist = fs.existsSync(pathForFile);
-    if (doesFileForCommandExist) {
-      try {
-        // fs.accessSync() will syncronously check if the file has exec permission
-        fs.accessSync(pathForFile, fs.constants.X_OK);
-        console.log(`${filename} is ${pathForFile}`);
-        return true;
-      } catch (err) {
-        // if the file doesn't have exec permission, the fs.accessSync() returns error which will be catched here offcourse.
-        // skip, do nothing
-      }
-    } else {
-      // if the file doesn't exist return false, by default it would return undefined which is also correct since undefined is falsy boolean.
-      return false;
+const checkIfFileForCommandExist = (command: string) => {
+  for (let eachPath in paths) {
+    const pathForFile = path.join(eachPath.toString(), command);
+    if (fs.existsSync(pathForFile)) {
+      return pathForFile;
     }
-  });
-  // if after searching the entire path , no file was found then:
-  if (!isThereAnyExecFile) console.log(`${filename}: not found`);
-  return;
+  }
+  return null;
 };
+
+const checkForExecPermission = (filePath: string) => {
+  try {
+    fs.accessSync(filePath, fs.constants.X_OK);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+// const checkExecFileExistAndPerm = (filename: string): boolean | undefined => {
+//   // some will return true as soon as it finds the file with the exec permission and doesn't check other paths , well that's how the path search for executable commands works.
+//   const isThereAnyExecFile = paths?.some((eachPath) => {
+//     // path.join() basically concats the parts of path as per the operating sysytem
+//     const pathForFile = path.join(eachPath.toString(), filename);
+
+//     // fs.existsSync() will synchronously check if the file exists.
+//     const doesFileForCommandExist = fs.existsSync(pathForFile);
+//     if (doesFileForCommandExist) {
+//       try {
+//         // fs.accessSync() will syncronously check if the file has exec permission
+//         fs.accessSync(pathForFile, fs.constants.X_OK);
+//         console.log(`${filename} is ${pathForFile}`);
+//         return true;
+//       } catch (err) {
+//         // if the file doesn't have exec permission, the fs.accessSync() returns error which will be catched here offcourse.
+//         // skip, do nothing
+//       }
+//     } else {
+//       return false;
+//     }
+//   });
+//   // if after searching the entire path , no file was found then:
+
+//   return isThereAnyExecFile;
+// };
 
 const runEcho = (commandParts: string[]): void => {
   console.log(commandParts.slice(1).join(" "));
@@ -53,7 +72,34 @@ const handleBuiltIn = (commandToCheck: string): boolean => {
 const runType = (typeArgument: string) => {
   const isBuiltIn = handleBuiltIn(typeArgument);
   if (!isBuiltIn) {
-    checkExecFileExistAndPerm(typeArgument);
+    const pathForFile = checkIfFileForCommandExist(typeArgument);
+    if (pathForFile) {
+      const hasExecPermission = checkForExecPermission(pathForFile);
+      if (hasExecPermission) {
+        console.log(`${typeArgument} is ${pathForFile}`);
+      } else {
+        // do nothing if file exist and doesn't have exec permission.
+      }
+    }
+    if (!pathForFile) console.log(`${typeArgument}: not found`);
+  }
+};
+
+const runCustomCommand = (command: string, args: String[]) => {
+  const pathForFile = checkIfFileForCommandExist(command);
+  if (pathForFile) {
+    const hasExecPermission = checkForExecPermission(pathForFile);
+    if (hasExecPermission) {
+      execFile(
+        pathForFile,
+        [command.toString(), ...args.map((eachArg) => eachArg.toString())],
+        (stdout) => {
+          console.log(stdout);
+        },
+      );
+    } else {
+      // do nothing if file exist and doesn't have exec permission.
+    }
   }
 };
 
@@ -71,6 +117,8 @@ function givePrompt() {
     } else if (commandParts[0] === "type") {
       commandParts[1] && runType(commandParts[1]);
     } else {
+      const [command, ...args] = commandParts;
+      runCustomCommand(command, args);
       console.log(`${commandParts[0]}: command not found`);
     }
     givePrompt();
