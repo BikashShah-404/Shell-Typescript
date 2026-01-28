ce5e734c44acbd919692f5ebfb36afa6e4870df0
BikashShah-404 <shahbikash4659@gmail.com>
2026-01-28 20:04:20 +0545

    builtin cd with absolute path

--- app/utils/runCd.ts
+++ app/utils/runCd.ts
@@ -2,8 +2,10 @@ import { statSync } from "node:fs";
 
 export const runCd = (dirToGo: string) => {
   try {
-    const doesDirExist = statSync(dirToGo).isDirectory();
-    if (doesDirExist) process.chdir(dirToGo);
+    // const doesDirExist = statSync(dirToGo).isDirectory();
+    // if (doesDirExist) {
+    process.chdir(dirToGo);
+    // }
   } catch (error) {
     console.log(`cd: ${dirToGo}: No such file or directory`);
   }
