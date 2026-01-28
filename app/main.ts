44dc69467c3002b05d100f8104b8d20437c56052
BikashShah-404 <shahbikash4659@gmail.com>
2026-01-28 11:39:12 +0545

    run a program

--- app/main.ts
+++ app/main.ts
@@ -86,16 +86,11 @@ const runType = (typeArgument: string) => {
 };
 
 const runCustomCommand = (command: string, args: String[]) => {
-  console.log("1");
-
   const pathForFile = checkIfFileForCommandExist(command);
-  console.log("2");
 
   if (pathForFile) {
     const hasExecPermission = checkForExecPermission(pathForFile);
     if (hasExecPermission) {
-      console.log("abc");
-
       execFile(
         pathForFile,
         [command.toString(), ...args.map((eachArg) => eachArg.toString())],
