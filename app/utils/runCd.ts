d994e0ddcdbb7c539b21ba97b2dcf3704fb55fd1
BikashShah-404 <shahbikash4659@gmail.com>
2026-01-28 20:22:19 +0545

    builtin cd with absolute, relative and home path

--- app/utils/runCd.ts
+++ app/utils/runCd.ts
@@ -1,11 +1,14 @@
 import { statSync } from "node:fs";
+import { homedir } from "node:os";
+import path from "node:path";
 
 export const runCd = (dirToGo: string) => {
   try {
-    // const doesDirExist = statSync(dirToGo).isDirectory();
-    // if (doesDirExist) {
+    if (dirToGo.startsWith("~")) {
+      const [home, ...restofPath] = dirToGo.split(path.sep);
+      dirToGo = path.join(homedir(), restofPath.join(path.sep));
+    }
     process.chdir(dirToGo);
-    // }
   } catch (error) {
     console.log(`cd: ${dirToGo}: No such file or directory`);
   }
