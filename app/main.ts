30d8f608e4e7beb63ef5c33b5084ebcedfafa452
BikashShah-404 <shahbikash4659@gmail.com>
2026-01-29 14:11:44 +0545

    limiting history entries

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,13 @@ import { runType } from "./utils/runType.ts";
 import { runExit } from "./utils/runExit.ts";
 import { runPwd } from "./utils/runPwd.ts";
 import { runCd } from "./utils/runCd.ts";
-import { commandHistory, runHistory } from "./utils/runHistory.ts";
+import {
+  commandHistory,
+  handleHistoryNavigation,
+  runHistory,
+} from "./utils/runHistory.ts";
+
+import readline from "readline";
 
 const rl = createInterface({
   input: process.stdin,
@@ -15,34 +21,36 @@ const rl = createInterface({
 rl.setPrompt("$ ");
 rl.prompt();
 
-function givePrompt() {
-  rl.on("line", (command: string) => {
-    // split(/s+/) - splits the string into parts wherever there are one or more whitespace characters.
-    commandHistory.push(command);
-    const commandParts = command.trim().split(/\s+/);
+rl.on("line", (command: string) => {
+  // split(/s+/) - splits the string into parts wherever there are one or more whitespace characters.
+  commandHistory.push(command);
+  const commandParts = command.trim().split(/\s+/);
 
-    if (commandParts[0] === "exit") {
-      runExit(commandParts, rl);
+  if (commandParts[0] === "exit") {
+    runExit(commandParts, rl);
+    return;
+  } else if (commandParts[0] === "echo") {
+    runEcho(commandParts);
+  } else if (commandParts[0] === "type") {
+    commandParts[1] && runType(commandParts[1]);
+  } else if (commandParts[0] === "pwd") {
+    runPwd();
+  } else if (commandParts[0] === "cd") {
+    runCd(commandParts[1]);
+  } else if (commandParts[0] === "history") {
+    runHistory(Number(commandParts[1]));
+  } else {
+    const [command, ...args] = commandParts;
+    if (command) {
+      const childproc = runCustomCommand(command, args);
+      childproc.on("close", () => rl.prompt());
       return;
-    } else if (commandParts[0] === "echo") {
-      runEcho(commandParts);
-    } else if (commandParts[0] === "type") {
-      commandParts[1] && runType(commandParts[1]);
-    } else if (commandParts[0] === "pwd") {
-      runPwd();
-    } else if (commandParts[0] === "cd") {
-      runCd(commandParts[1]);
-    } else if (commandParts[0] === "history") {
-      runHistory(Number(commandParts[1]));
-    } else {
-      const [command, ...args] = commandParts;
-      if (command) {
-        const childproc = runCustomCommand(command, args);
-        childproc.on("close", () => rl.prompt());
-        return;
-      }
     }
-    rl.prompt();
-  });
-}
-givePrompt();
+  }
+  rl.prompt();
+});
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+handleHistoryNavigation(rl);
--- app/utils/runHistory.ts
+++ app/utils/runHistory.ts
@@ -1,3 +1,5 @@
+import type { Interface } from "readline";
+
 export const commandHistory: string[] = [];
 
 export const runHistory = (length: number) => {
@@ -12,4 +14,41 @@ export const runHistory = (length: number) => {
       }
     });
   }
+};
+
+let historyIndex = -1;
+export const handleHistoryNavigation = (rl: Interface) => {
+  process.stdin.on("keypress", (_, key) => {
+    if (!key) return;
+
+    if (key.name === "up") {
+      if (commandHistory.length === 0) return;
+
+      if (historyIndex === -1) {
+        historyIndex = commandHistory.length - 1;
+      } else if (historyIndex > 0) {
+        historyIndex -= 1;
+      }
+
+      // It clears the current input, whatever was written on terminal before pressing up
+      rl.write(null, { ctrl: true, name: "u" });
+      //   Now we will write the history:
+      rl.write(commandHistory[historyIndex]);
+    }
+
+    if (key.name === "down") {
+      if (historyIndex === -1) return;
+
+      historyIndex++;
+
+      if (historyIndex >= commandHistory.length) {
+        historyIndex = -1;
+        rl.write(null, { ctrl: true, name: "u" });
+        return;
+      }
+
+      rl.write(null, { ctrl: true, name: "u" });
+      rl.write(commandHistory[historyIndex]);
+    }
+  });
 };
